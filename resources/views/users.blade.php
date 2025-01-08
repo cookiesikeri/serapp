38c3b60a9e901b55d33f4cfdd866d1ebd51b0b4d
cookiesikeri <ebenezer.ikeri@bankly.ng>
2025-01-08 23:55:53 +0100

    users

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Searchable Table</title>
+    <title>All Users Table</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
